328a283fe10b4ab7481dd9e4f68dfbbc7d588f71
Stephen Malina <stephenmalina@gmail.com>
2020-02-10 16:00:33 -0500

    Get closer to actually running MR

--- scr/copy_from_gcloud.sh
+++ scr/copy_from_gcloud.sh
@@ -4,3 +4,6 @@ gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse gregor
 if [[ $1 == "-d" ]]; then
     gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse gregor-2-vm:~/project/dat/ ./dat/ 
 fi
+if [[ $1 == "-r" ]]; then
+    gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse gregor-2-vm:~/project/dat/means_and_uncertainties.csv ./dat/
+fi
