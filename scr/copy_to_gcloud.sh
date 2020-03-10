30eee8fb9017b0c7bdd42d1902b1bbaf42f86055
Stephen Malina <stephenmalina@gmail.com>
2020-03-10 11:21:59 -0400

    Update scripts

--- scr/copy_to_gcloud.sh
+++ scr/copy_to_gcloud.sh
@@ -1,8 +1,14 @@
-gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/*.py gregor-2-vm:~/project/src/
-gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/*.ipynb gregor-2-vm:~/project/src/
-gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/data/*.py gregor-2-vm:~/project/src/data/
-gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/pyx/*.py gregor-2-vm:~/project/src/pyx/
-gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/pyx/*.pyx gregor-2-vm:~/project/src/pyx/
+if [[ $1 == "-n" ]]; then
+    gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/*.ipynb gregor-2-vm:~/project/src/
+fi
+if [[ $1 == "-p" ]]; then
+    gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/*.py gregor-2-vm:~/project/src/
+    gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/data/*.py gregor-2-vm:~/project/src/data/
+fi
+if [[ $1 == "-c" ]]; then
+    gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/pyx/*.py gregor-2-vm:~/project/src/pyx/
+    gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./src/pyx/*.pyx gregor-2-vm:~/project/src/pyx/
+fi
 if [[ $1 == "-d" ]]; then
     gcloud compute scp --project coms-4995-260215 --zone us-west1-b --recurse ./dat/ gregor-2-vm:~/project/
 fi
