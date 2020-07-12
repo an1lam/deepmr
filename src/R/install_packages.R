89ac20ff6461293061b1bdf2ee3dd6e8b7bb5c17
Stephen Malina <stephenmalina@gmail.com>
2020-07-12 10:33:16 -0400

    Make installation reproducible-ish

---
+++ src/R/install_packages.R
@@ -0,0 +1,8 @@
+# R must-haves
+install.packages("tidyverse")
+install.packages("dplyr")
+
+# 3rd party packages
+install.packages(c("plotly", "iterpc"))
+install.packages("MendelianRandomization")
+install.packages("ks")
