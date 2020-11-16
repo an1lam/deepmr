ca4befebbb64dcc74d2ee5492c1ca9176c2048db
Stephen Malina <stephenmalina@gmail.com>
2020-11-16 11:32:05 -0500

    Calibrate confidence intervals
    And automate / consolidate R analysis in the process.

--- src/R/install_packages.R
+++ src/R/install_packages.R
@@ -1,13 +1,13 @@
 # Pre-requisites
-install.packages(c("readr", "iterpc", "plotly", "mvtnorm"))
+install.packages(c("readr", "iterpc", "plotly", "mvtnorm", "codetools"))
 
 # R must-haves
 install.packages("tidyverse")
 install.packages("dplyr")
 
 # 3rd party packages
-install.packages(c("plotly", "iterpc"))
 install.packages("MendelianRandomization")
 install.packages("ks")
 install.packages("ggplot2")
 install.packages("stringr")
+install.packages("foreach")
