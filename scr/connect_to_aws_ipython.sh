6030c3070cf6d7f074272d3bb83af3e59e800ae0
Stephen Malina <stephenmalina@gmail.com>
2021-01-31 08:57:31 -0500

    Update AWS box URL

--- scr/connect_to_aws_ipython.sh
+++ scr/connect_to_aws_ipython.sh
@@ -1,4 +1,4 @@
 PEM_FILE_PATH=${1:-"~/StephenDAK_Dell.pem"}
-EC2_DNS=${2:-"ec2-18-207-142-197.compute-1.amazonaws.com"}
+EC2_DNS=${2:-"ec2-34-203-38-98.compute-1.amazonaws.com	"}
 PORT=${3:-8888}
 ssh -i $PEM_FILE_PATH ubuntu@$EC2_DNS -L $PORT:localhost:$PORT
