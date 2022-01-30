63b128a1869fe3d20c677c1ebc560415271c1fd1
Stephen Malina <stephenmalina@gmail.com>
2022-01-29 19:42:21 -0500

    Remove old scripts

--- scr/drop_extraneous_bed_columns.sh
+++
@@ -1,1 +0,0 @@
-find $PROJECT_DIR/dat/ -name *.bed.gz -exec .venv/bin/python data/drop_extraneous_bed_columns.py 3 {} \;
--- scr/give_ipython_data_access.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-sudo groupadd dataacc
-sudo usermod -a -G dataacc jupyter
-sudo usermod -a -G dataacc stephenmalina
